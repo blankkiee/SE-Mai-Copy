2ef5fa2b82d7e420140aeab36dbc4b17bca0ed82
MarkDaDave <mdmartin983@gmail.com>
2024-04-05 02:45:05 +0800

    conflict merge!!

--- app/Http/Controllers/AgentController.php
+++ app/Http/Controllers/AgentController.php
@@ -2,17 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Support\Facades\Hash;
 
 class AgentController extends Controller
 {
      public function AgentDashboard(){
 
-        return view('agent.index');
+        return view('agent.agent_dashboard');
     }// End Method
 
     public function agentlogout(Request $request): RedirectResponse
@@ -29,9 +27,4 @@ class AgentController extends Controller
      public function AgentLogin(){
         return view('agent.agent_login');
     }
-
-
-   
-
- 
 }
